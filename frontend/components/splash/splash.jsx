e9625081d9eed6ad7004bf1f8bda82841ebd6481
tieulam <ttl.t954@gmail.com>
2020-06-05 12:38:43 -0600

    Completed splash page styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Playlist from '../tracks/playlist_container';
 
@@ -11,7 +10,7 @@ class Splash extends React.Component {
       <section className="splash-top">
         <div className="splash-top-nav">
           <div className="splash-top-nav-left">
-            <img src="" alt="wavetape-logo" />
+            <img className="stnl-logo" src="assets/wt-icon.png" alt="wavetape-logo" />
           </div>
           <div className="spash-top-nav-right">
             <button className="session-btn signin" onClick={() => openModal('login')}>Sign in</button>
@@ -27,9 +26,21 @@ class Splash extends React.Component {
       </section>
 
       <section className="splash-music">
-        Music coming soon
-        {/* <Playlist />
-        <Playlist /> */}
+        <div className="splash-music-search">
+          <div className="sms-container">
+            <form className="sms-searchbar-form">
+              <input className="sms-searchbar"
+                type="search"
+                placeholder="Search for artist, bands, tracks, tapes" /> 
+              <button type="button" className="sms-searchbar-button">Search</button>
+            </form>
+             <span>or</span>
+            <a className="sms-upload">Upload your own</a>
+          </div>
+        </div>
+        <div className="splash-music-trending">
+          Music coming soon
+        </div>
       </section>
 
       <section className="splash-devices">
@@ -47,12 +58,42 @@ class Splash extends React.Component {
 
       <section className="splash-creative">
         <div className="splash-creative-content">
-          <p className="test">Calling all creators</p>
-          <p>Get on SoundCloud to connect with fans, share your sounds, and grow your audience. What are you waiting for?</p>
+          <p className="scc-title">Calling all creators</p>
+          <p className="scc-tagline">Get on WaveTape to set sail, share your sounds, and ride the waves. Darling, what are you waiting for?</p>
+          <div><a className="scc-button not-allowed">Find out more</a></div>
         </div>
       </section>
 
       <section className="splash-bottom">
+        <div className="splash-botttom-session">
+          <p className="sbs-title">Thanks for listening. Now join in.</p>
+          <p className="sbs-tagline">Save tracks, follow artists and build playlists. All for free.</p>
+          <p><button className="button-large sbs-signup" onClick={() => openModal('signup')}>Create Account</button></p>
+          <p className="sbs-alternative font-light">Already have an account?
+            <button className="button-large sbs-signin" onClick={() => openModal('login')}>Sign in</button>
+          </p>
+        </div>
+      </section>
+     
+      <section className="splash-footer">
+        <div className="splash-footer-links not-allowed">
+          <a>Popular searches</a>&nbsp;⁃
+          <a> Directory</a>&nbsp;⁃
+          <a> About us</a>&nbsp;⁃
+          <a> Creator Resources</a>&nbsp;⁃
+          <a> Blogs</a>&nbsp;⁃
+          <a> Jobs</a>&nbsp;⁃
+          <a> Developers</a>&nbsp;⁃
+          <a> Help</a>&nbsp;⁃
+          <a> Legal</a>&nbsp;⁃
+          <a> Privacy</a>&nbsp;⁃
+          <a> Cookies</a>&nbsp;⁃
+          <a> Imprints</a>&nbsp;⁃
+          <a> Charts</a>
+        </div>
+        <div className="splash-footer-lang not-allowed">
+           <p><a className="sfl-link">Language: </a>English (US)</p>
+        </div>
       </section>
      </div>
    )
