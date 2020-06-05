25102a5eeced086764e4fff096392a68de4dd979
tieulam <ttl.t954@gmail.com>
2020-06-05 07:35:58 -0600

    Added demo user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Follow.destroy_all
+User.destroy_all
+
+User.create!({email: "tt@demo.com", password: "strawberries", username: "demo"})
