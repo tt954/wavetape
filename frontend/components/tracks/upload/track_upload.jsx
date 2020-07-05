cc794524c84e5676b402a0db11412e7a802ca05f
tieulam <ttl.t954@gmail.com>
2020-07-04 23:14:51 -0600

    style upload form

--- frontend/components/tracks/upload/track_upload.jsx
+++ frontend/components/tracks/upload/track_upload.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import NavBar from  '../../nav_bar/nav_bar_container';
 import TrackUploadForm from './track_upload_form';
+import { FaChevronDown } from 'react-icons/fa';
 
 class TrackUpload extends React.Component {
   constructor(props) {
@@ -42,12 +43,43 @@ class TrackUpload extends React.Component {
           <div className="upload-nav"></div>
 
           <div className="upload-main">
-            <div className="ulm-tryPro"></div>
+            <div className="ulm-tryPro">
+              <div className="ulm-tryPro-left">
+                <div className="upload-limit">
+                  <p>0% of free uploads used</p>
+                  <FaChevronDown />
+                </div>
+                <div className="upload-limit-meter"></div>
+                <p>Try Pro for unlimited uploads.</p>
+              </div>
+              <div className="ulm-tryPro-right">
+                <button>Try Pro Unlimited</button>
+              </div>
+            </div>
             <TrackUploadForm
               update={this.update}
               handleFile={this.handleFile}
             />
-            <div className="ulm-footer"></div>
+            <div className="ulm-footer">
+              <div className="ulm-footer-top">
+                <p>
+                  Supported file types and sizes ⁃ Upload troubleshooting tips ⁃
+                  Copyright FAQs
+                </p>
+                <p>
+                  By uploading, you confirm that your sounds comply with the
+                  <span> Copyright Terms</span> and you don't infringe anyone
+                  else's rights.
+                </p>
+              </div>
+              <div className="ulm-footer-bottom">
+                <p>
+                  Legal ⁃ Privacy ⁃ Cookies ⁃ Imprint ⁃ Creator Resources ⁃ Blog
+                  ⁃ Charts ⁃ Popular searches
+                </p>
+                <p>Visit site's Github page</p>
+              </div>
+            </div>
           </div>
 
           <div className="upload-footer"></div>
--- frontend/components/tracks/upload/track_upload_form.jsx
+++ frontend/components/tracks/upload/track_upload_form.jsx
@@ -4,14 +4,19 @@ class TrackUploadForm extends React.Component {
   render() {
     return (
       <div className="ulm-dropzone">
-        <span>Drag and drop your track here</span>
-        <input
-          type="file"
-          id="trackUpload"
-          className="inputfile"
-          onChange={this.props.handleFile}
-        />
-        <label for="trackUpload">or choose file to upload</label>
+        <div className="ulm-dropzone-top">
+          <span>Drag and drop your track here</span>
+          <input
+            type="file"
+            id="trackUpload"
+            className="inputfile"
+            onChange={this.props.handleFile}
+          />
+          <label for="trackUpload">or choose file to upload</label>
+        </div>
+        <div className="ulm-dropzone-bottom">
+          <p>Provide FLAC, WAV, ALAC, or AIFF for highest audio quality. Learn more about lossless HD.</p>
+        </div>
       </div>
     );
   }
